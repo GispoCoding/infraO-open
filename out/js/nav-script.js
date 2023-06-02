0a3103bf0f4730cfffd941ff4a8bed6d722d1cc2
Juho Ervasti <118284595+JuhoErvasti@users.noreply.github.com>
2023-06-02 15:29:13 +0300

    Delete out directory

--- out/js/nav-script.js
+++
@@ -1,6 +0,0 @@
-function navToggle() {
-  let sidebar = document.getElementById('sidebar');
-  sidebar.classList.toggle('show');
-  let hamburger = document.querySelector('.hamburger')
-  hamburger.classList.toggle('is-active');
-}
